cb2063b7726cdb9e069d9d5723f9803e02dfdff9
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-11-05 19:39:26 +0800

    Add search serverless functions

---
+++ functions/search-milk.js
@@ -0,0 +1,28 @@
+const qs = require('querystring');
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  const response = await fetch(`http://api.unsplash.com/search/photos?query=milk`,
+    {
+        method: 'GET',
+        headers: {
+            Authorization: `Client-ID ${process.env.UNSPLASH_API_TOKEN}`,
+        },
+    }
+  ).then((response) => response.json())
+  .catch((error) => console.log(error));
+
+  const firstResult = response.results[0];
+
+  return {
+    statusCode: 200,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    // display image 
+    body: `<img
+        src="${firstResult.urls.regular}"
+        alt="${firstResult.alt_description}"
+    /><p>No worries, here is some milk for you to drink!</p>`,
+  };
+};
